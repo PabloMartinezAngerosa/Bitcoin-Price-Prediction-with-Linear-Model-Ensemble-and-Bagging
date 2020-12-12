6c2bfece6a217d0e8313dfb9f1ad98d3e82add18
PabloMartinezAngerosa <pablomartinezangerosa@gmail.com>
2020-12-11 22:32:45 -0300

    bagging

--- Main.R
+++ Main.R
@@ -34,38 +34,38 @@ totalEstimadoresEnBandas/base::nrow(databaseFrameCombEstimators)
 
 # Calcula RMSE de la media experta de opiniones como estimador
 acumMeanComboEstimadores<- 0
-for (i in base::c(1:base::nrow(databaseFrameCombEstimatorsTrain))) {
-  close <- databaseFrameCombEstimatorsTrain[i,ncol(databaseFrameCombEstimatorsTrain)]
+for (i in base::c(1:base::nrow(databaseFrameCombEstimatorsTest))) {
+  close <- databaseFrameCombEstimatorsTest[i,ncol(databaseFrameCombEstimatorsTest)]
   acumCloseEstimated  <- 0
-  for (j in base::c(1:(base::ncol(databaseFrameCombEstimatorsTrain)-1))) {
-    closeEstimated <- databaseFrameCombEstimatorsTrain[i, j]
+  for (j in base::c(1:(base::ncol(databaseFrameCombEstimatorsTest)-1))) {
+    closeEstimated <- databaseFrameCombEstimatorsTest[i, j]
     acumCloseEstimated <- acumCloseEstimated + closeEstimated
   }
-  meanEstimator <- acumCloseEstimated/(base::ncol(databaseFrameCombEstimatorsTrain)-1)
+  meanEstimator <- acumCloseEstimated/(base::ncol(databaseFrameCombEstimatorsTest)-1)
   acumMeanComboEstimadores <- acumMeanComboEstimadores + ( meanEstimator - close )^2
 }
 RMSEMeanComboEstimadores <- base::sqrt(
                               acumMeanComboEstimadores / 
-                              base::nrow(databaseFrameCombEstimatorsTrain) 
+                              base::nrow(databaseFrameCombEstimatorsTest) 
                             )
 RMSEMeanComboEstimadores
 
 # Utilizamos Bagging para media experta de opinones como estimador
 # Se recomienda un 60% de las observaciones por bolsa
-nInBag = base::nrow(databaseFrameCombEstimatorsTrain)  * 0.6
+nInBag = (ncol(databaseFrameCombEstimatorsTest)  - 1 )  * 0.6
 # Generamos 1000 bolsas
 nBag <- 1000
 acumRMSEBagging <- 0
 
-for (i in base::c(1:base::nrow(databaseFrameCombEstimatorsTrain))) {
-  close <- databaseFrameCombEstimatorsTrain[i,ncol(databaseFrameCombEstimatorsTrain)]
-  data  <- databaseFrameCombEstimatorsTrain[i, 1:(ncol(databaseFrameCombEstimatorsTrain)-1)]   
+for (i in base::c(1:base::nrow(databaseFrameCombEstimatorsTest))) {
+  close <- databaseFrameCombEstimatorsTest[i,ncol(databaseFrameCombEstimatorsTest)]
+  data  <- databaseFrameCombEstimatorsTest[i, 1:(ncol(databaseFrameCombEstimatorsTest)-1)]   
   baggingPrediction <- DoBagging(data, nBag, nInBag)
   acumRMSEBagging   <- acumRMSEBagging  + ( baggingPrediction - close )^2
   print(i)
 }
 RMSEBaggingComboEstimadores <- base::sqrt(
-  acumRMSEBagging / base::nrow(databaseFrameCombEstimatorsTrain) 
+  acumRMSEBagging / base::nrow(databaseFrameCombEstimatorsTest) 
 )
 RMSEBaggingComboEstimadores
 
